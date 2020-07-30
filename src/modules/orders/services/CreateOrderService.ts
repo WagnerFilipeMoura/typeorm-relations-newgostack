1a0c506cf267e62184e2b4d3a003e7269c401ed4
WagnerFilipeMoura <37957196+WagnerFilipeMoura@users.noreply.github.com>
2020-07-29 22:56:46 -0300

    Initial commit

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,70 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+@injectable()
+class CreateProductService {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+    const findProducts = await this.productsRepository.findAllById(products);
+    if (!findProducts) {
+      throw new AppError('Products not found');
+    }
+    const findProductsInStock = products.every(product => {
+      const findedProduct = findProducts.find(
+        storedProduct => storedProduct.id === product.id,
+      );
+      return product.quantity < (findedProduct?.quantity || 0);
+    });
+    if (!findProductsInStock) {
+      throw new AppError('Product not in stock');
+    }
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: products.map(product => {
+        const findProduct = findProducts.find(
+          storedProduct => storedProduct.id === product.id,
+        );
+        return {
+          product_id: product.id,
+          quantity: product.quantity,
+          price: findProduct?.price || 0,
+        };
+      }),
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
+  }
+}
+
+export default CreateProductService;
